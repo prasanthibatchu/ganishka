1668f1a0875130262c9576f76e17156fc4c7024d
prasanthibatchu <batchu.prasanthi916@gmail.com>
2022-06-06 11:28:15 +0530

    signup page changed

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,5 +1,5 @@
 import React,{useContext, useState} from 'react'
-import {Container,Box,Button,Grid,TextField,FormControl,RadioGroup,FormControlLabel,Radio,Checkbox,FormGroup} from '@mui/material'
+import {Container,Box,Button,Grid,TextField,FormControl,RadioGroup,FormControlLabel,Radio,Checkbox,FormGroup,FormLabel} from '@mui/material'
 import { UserContext,SnackContext } from './context'
 
 
@@ -83,7 +83,7 @@ export const Signup=()=>{
             })
         }
         else if(user.pass.length <6){
-            setErr(6)
+            setErr(7)
             // alert("please enter your password")
             setSnack({
                 message:"please enter your password",
@@ -93,7 +93,7 @@ export const Signup=()=>{
             })
         }
         else if(user.cpass.length <6){
-            setErr(6)
+            setErr(8)
             // alert("please confirm password")
             setSnack({
                 message:"please confirm password",
@@ -103,7 +103,7 @@ export const Signup=()=>{
             })
         }
         else if (user.pass !== user.cpass){
-            setErr(6)
+            
             // alert("mismatch password")
             setSnack({
                 message:"password mismatch",
@@ -128,9 +128,7 @@ export const Signup=()=>{
     }
     const onChangeValue=(e)=>{
         setUser({...user,[e.target.name]:e.target.value})
-    }
-   
-  
+    }  
     return(
         <div>
         <h1>Registration Form</h1>
@@ -141,48 +139,99 @@ export const Signup=()=>{
                 direction="column"
                 justifyContent="space-between"
                 alignItems="stretch"
-                  mt={3}         
+                  mt={3} 
+                  spacing={2}        
                 >
-                    <Grid container spacing={2}>
-                    <Grid item xs={12}>
+                    <Grid item spacing={12}>
+                    <Grid
+                        container
+                        direction="row"
+                        justifyContent="space-between"
+                        alignItems="center"
+                        spacing={1}
+                    >
+                        
+                    <Grid item xs={6}>
                         <TextField  label="Name" variant="outlined" name='name' value={user.name} error={err ===1 && true} onChange={onChangeValue}/>
                     </Grid>
-                    <Grid item xs={12}>
+                    <Grid item xs={6}>
                         <TextField  label="Email" variant="outlined"  name='email' value={user.email} error={err ===2 && true} onChange={onChangeValue}/>
                     </Grid>
-                    <Grid item xs={12}>
+                    </Grid>
+                    </Grid>
+                    <Grid item spacing={12}>
+                    <Grid
+                        container
+                        direction="row"
+                        justifyContent="space-between"
+                        alignItems="center"
+                        spacing={1}
+                    >
+                        
+                    <Grid item xs={6}>
+                    
                         <TextField  label="Phone" variant="outlined" name='phone' value={user.phone} error={err ===3 && true} onChange={onChangeValue} />
                     </Grid>
-                    <Grid item xs={12}>
+                    <Grid item xs={6}>
                         <TextField  label="Address" variant="outlined" maxRows={4}  multiline name='address' value={user.address} error={err ===4 && true} onChange={onChangeValue} />
+                    </Grid>
+                    </Grid>
                     </Grid>
                     <Grid item xs={12}>
                     <Grid>
                          <FormControl>
-                                
+                        
                                 <RadioGroup
                                      row
                                      name='gender'
                                      value={user.gender}
                                     onChange={onChangeValue}
-                                  >
+                               
+                                   
+                                  > 
+                                  <div>
+                                  <FormLabel>Gender</FormLabel>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                   
                                     <FormControlLabel value="female" control={<Radio />} label="Female" />
-                                    <FormControlLabel value="male" control={<Radio />} label="Male" />                                                                        
+                                    <FormControlLabel value="male" control={<Radio />} label="Male" />         </div>                                                               
                                 </RadioGroup>
                            </FormControl>
-                               
+                      </Grid>         
                     </Grid>  
+                    <Grid item spacing={12}>
+                    <Grid
+                        container
+                        direction="row"
+                        justifyContent="space-between"
+                        alignItems="center"
+                        spacing={1}
+                    >
                         
+                    <Grid item xs={6}>  
                     <TextField label="city" variant="outlined" name='city' value={user.city} error={err ===5 && true} onChange={onChangeValue} />
                     </Grid>
-                    <Grid item xs={12}>
+                    <Grid item xs={6}>
                         <TextField  label="State" variant="outlined" name='state' value={user.state} error={err ===6 && true} onChange={onChangeValue} />
                     </Grid>
-                    <Grid item xs={12}>
+                    </Grid>
+                    </Grid>
+                    <Grid item spacing={12}>
+                    <Grid
+                        container
+                        direction="row"
+                        justifyContent="space-between"
+                        alignItems="center"
+                        spacing={1}
+                    >
+                        
+                    <Grid item xs={6}>  
+                    
                         <TextField  label="password" variant="outlined" name='pass' value={user.pass} error={err ===7 && true} onChange={onChangeValue} />
                     </Grid>
-                    <Grid item xs={12}>
+                    <Grid item xs={6}>
                         <TextField  label="confirm password" variant="outlined" name='cpass' value={user.cpass} error={err ===8 && true} onChange={onChangeValue} />
+                    </Grid>
+                    </Grid>
                     </Grid>
                     <Grid item xs={12}>
                         <FormControl>
@@ -202,8 +251,8 @@ export const Signup=()=>{
 
                     <Grid item xs={12}>
                     <Button variant='contained' onClick={Valid}>Submit</Button>
-                    </Grid>
-                </Grid></Grid>
+                   </Grid> 
+                </Grid>
             </Box>
         </Container>
         </div>
